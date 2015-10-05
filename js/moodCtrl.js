818880bdbf4154e9807c2bc4ee886aac2e40a1c7
Trevor Thomas <ttrev22@gmail.com>
2015-10-05 16:41:04 -0500

    created soundService, moodCtrl, and implemented search into view

--- js/moodCtrl.js
+++ js/moodCtrl.js
@@ -1,3 +1,16 @@
-app.controller('MoodCtrl', function() {
+app.controller('MoodCtrl', function(soundService) {
+	var ctrl = this;
+	ctrl.getUser = function () {
+		soundService.getUserTracks(ctrl.username).then(function (data){
+			ctrl.username = '';
+			ctrl.userTracks = data;
+		})
+	}
 
+	ctrl.play = function (track_url){
+		SC.oEmbed(track_url, { auto_play: true}, function(oembed){
+			debugger;
+			ctrl.$apply(ctrl.iFrame = $sce.trustAsHtml(oembed.html));
+		});
+	}
 });
--- js/service.js
+++ js/service.js
@@ -1,11 +1,13 @@
-app.factory('playlistService', [function() {
-	var moods = ['happy', 'sleepy'];
+app.service('soundService', function ($http, $q) {
 	
-	return {
-		list: function() {
-			return moods;
-		}
-	};
+	var url = "http://api.soundcloud.com/users/";
+	this.getUserTracks = function (user) {
+		var deferred = $q.defer();
+		$http.get(url + user + '/tracks.json?client_id=5c43e7e2b881c2eae59baf7fae3808e3').then(function (response) {
+			deferred.resolve(response.data);
+		})
+		return deferred.promise;
+	}
 
-}]);
+});
 
