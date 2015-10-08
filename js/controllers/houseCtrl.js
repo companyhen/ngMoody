e1a97ed79222f29578923f2a7e3d9a1688f15892
companyhen <companyhen@gmail.com>
2015-10-08 15:22:55 -0400

    Fixed indexing on clickHandler for prev/next button

--- js/controllers/houseCtrl.js
+++ js/controllers/houseCtrl.js
@@ -18,14 +18,13 @@ app.controller('HouseCtrl', function($scope, soundService, $sce, $timeout, clien
 		ctrl.url = $sce.trustAsResourceUrl(result[1].data[ctrl.playerInfo.song].stream_url + client_id);
 	});
 
-	ctrl.clickHandler = function (track) {
+	ctrl.clickHandler = function (track, index) {
 		//ctrl.url = null;
 		$timeout(function(){
-			// console.log($sce.trustAsResourceUrl(track.stream_url + client_id));
 			ctrl.url = $sce.trustAsResourceUrl(track.stream_url + client_id);
-			console.log(track);
 			ctrl.isStreaming = true;
-		})		
+		})
+		ctrl.playerInfo.song = index;
 	};
 
 	ctrl.play = function () {
--- js/service.js
+++ js/service.js
@@ -1,9 +1,18 @@
 app.service('soundService', function ($http, $q, client_id) {
 
 	var genres = [
-		{name: "rock", moods: ""},
-		{name: "house", moods: ""},
-		{name: "hip-hop", moods: ""}
+		{name: "rock", moods: ""}, // 0
+		{name: "house", moods: ""}, // 1
+		{name: "hip-hop", moods: ""}, // 2
+		{name: "relax", moods: ""}, // 3
+		{name: "relax", moods: ""}, // 4
+		{name: "relax", moods: ""}, // 5
+		{name: "relax", moods: ""}, // 6
+		{name: "relax", moods: ""}, // 7
+		{name: "relax", moods: ""}, // 8
+		{name: "relax", moods: ""}, // 9
+		{name: "relax", moods: ""}, // 10
+		{name: "relax", moods: ""}, // 11
 	];
 
 	var genreData = [];
@@ -23,7 +32,7 @@ app.service('soundService', function ($http, $q, client_id) {
 		for(i=0; i<genres.length; i++){
 			var genre = genres[i].name;
 			var moods = genres[i].moods;
-			genreData[i] = $http.get('http://api.soundcloud.com/tracks.json' + client_id + '&q=' + moods + '&genres=' + genre + '&limit=20&duration[from]=100000&duration[to]=400000&order=hotness');
+			genreData[i] = $http.get('http://api.soundcloud.com/tracks.json' + client_id + '&q=' + moods + '&genres=' + genre + '&limit=20&duration[from]=100000&duration[to]=400000&streamable=true');
 		}
 		return $q.all(genreData);
 	}
